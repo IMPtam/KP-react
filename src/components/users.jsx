eb52289eb03914b2db310c407788de7291e00057
impus <imp-tam@ya.ru>
2022-09-18 20:51:39 +0300

    task 1 teacher's decision

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,49 +3,54 @@ import api from "../api";
 
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
+
   const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== userId));
+    setUsers(users.filter((user) => user._id !== userId));
   };
-  const renderPhrase = () => {
-    let x = users.length;
-    console.log(x);
-    if (x === 0) return "Никто с тобой не тусанет";
-    if (x === 2 || x === 3 || x === 4) {
-      return x + " Человекa тусанет с тобой";
-    } else {
-      return x + " Человек тусанет с тобой";
-    }
-  };
-  const getBageClasses = () => {
-    let classes = "fs-3 fw-bold badge ";
-    classes += users.length !== 0 ? "bg-primary" : "bg-danger";
-    return classes;
+
+  const renderPhrase = (number) => {
+    const lastOne = Number(number.toString().slice(-1));
+    if (number > 4 && number < 15) return "человек тусанет";
+    if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+    if (lastOne === 1) return "человек тусанет";
+    return "человек тусанет";
   };
 
   return (
     <>
-      <span className={getBageClasses()}>{renderPhrase()}</span>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качество</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
+      <h2>
+        <span
+          className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+        >
+          {users.length > 0
+            ? `${
+                users.length + " " + renderPhrase(users.length)
+              } с тобой сегодня`
+            : "Никто с тобой не тусанет"}
+        </span>
+      </h2>
+
+      {users.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
               <tr key={user._id}>
                 <td>{user.name}</td>
                 <td>
                   {user.qualities.map((item) => (
                     <span
-                      key={item._id}
                       className={"badge m-1 bg-" + item.color}
+                      key={item._id}
                     >
                       {item.name}
                     </span>
@@ -53,21 +58,20 @@ const Users = () => {
                 </td>
                 <td>{user.profession.name}</td>
                 <td>{user.completedMeetings}</td>
-                <td>{user.rate}</td>
+                <td>{user.rate} /5</td>
                 <td>
                   <button
-                    type="button"
-                    className="btn btn-danger"
                     onClick={() => handleDelete(user._id)}
+                    className="btn btn-danger"
                   >
-                    Удалить
+                    delete
                   </button>
                 </td>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };

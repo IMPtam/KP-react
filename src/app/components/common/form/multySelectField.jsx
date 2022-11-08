ef84f1e18517c4f516d67ab0f836e0509a67a7fd
impus <imp-tam@ya.ru>
2022-11-08 15:50:24 +0300

    bug with multiselect

--- src/app/components/common/form/multySelectField.jsx
+++ src/app/components/common/form/multySelectField.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Select, { StylesConfig } from "react-select";
+import Select from "react-select";
 import PropTypes from "prop-types";
 
 const MultySelectField = ({ options, onChange, name, label, defaultValue }) => {
@@ -21,57 +21,57 @@ const MultySelectField = ({ options, onChange, name, label, defaultValue }) => {
     const handleChange = (value) => {
         onChange({ name: name, value });
     };
-    const dot = (color = "transparent") => ({
-        alignItems: "center",
-        display: "flex",
+    // const dot = (color = "transparent") => ({
+    //     alignItems: "center",
+    //     display: "flex",
 
-        ":before": {
-            backgroundColor: color,
-            borderRadius: 10,
-            content: '" "',
-            display: "block",
-            marginRight: 8,
-            height: 10,
-            width: 10
-        }
-    });
+    //     ":before": {
+    //         backgroundColor: color,
+    //         borderRadius: 10,
+    //         content: '" "',
+    //         display: "block",
+    //         marginRight: 8,
+    //         height: 10,
+    //         width: 10
+    //     }
+    // });
 
-    const colourStyles: StylesConfig<defaultValueArray, true> = {
-        control: (styles) => ({ ...styles, backgroundColor: "white" }),
-        option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-            const color = chroma(data.color);
-            return {
-                ...styles,
-                backgroundColor: isDisabled
-                    ? undefined
-                    : isSelected
-                    ? data.color
-                    : isFocused
-                    ? color.alpha(0.1).css()
-                    : undefined,
-                color: isDisabled
-                    ? "#ccc"
-                    : isSelected
-                    ? chroma.contrast(color, "white") > 2
-                        ? "white"
-                        : "black"
-                    : data.color,
-                cursor: isDisabled ? "not-allowed" : "default",
+    // const colourStyles: StylesConfig<defaultValueArray, true> = {
+    //     control: (styles) => ({ ...styles, backgroundColor: "white" }),
+    //     option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    //         const color = chroma(data.color);
+    //         return {
+    //             ...styles,
+    //             backgroundColor: isDisabled
+    //                 ? undefined
+    //                 : isSelected
+    //                 ? data.color
+    //                 : isFocused
+    //                 ? color.alpha(0.1).css()
+    //                 : undefined,
+    //             color: isDisabled
+    //                 ? "#ccc"
+    //                 : isSelected
+    //                 ? chroma.contrast(color, "white") > 2
+    //                     ? "white"
+    //                     : "black"
+    //                 : data.color,
+    //             cursor: isDisabled ? "not-allowed" : "default",
 
-                ":active": {
-                    ...styles[":active"],
-                    backgroundColor: !isDisabled
-                        ? isSelected
-                            ? data.color
-                            : color.alpha(0.3).css()
-                        : undefined
-                }
-            };
-        },
-        input: (styles) => ({ ...styles, ...dot() }),
-        placeholder: (styles) => ({ ...styles, ...dot("#ccc") }),
-        singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) })
-    };
+    //             ":active": {
+    //                 ...styles[":active"],
+    //                 backgroundColor: !isDisabled
+    //                     ? isSelected
+    //                         ? data.color
+    //                         : color.alpha(0.3).css()
+    //                     : undefined
+    //             }
+    //         };
+    //     },
+    //     input: (styles) => ({ ...styles, ...dot() }),
+    //     placeholder: (styles) => ({ ...styles, ...dot("#ccc") }),
+    //     singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) })
+    // };
     return (
         <div className="mb-4">
             <label htmlFor="validationCustom04" className="form-label">
@@ -85,7 +85,7 @@ const MultySelectField = ({ options, onChange, name, label, defaultValue }) => {
                 options={optionsArray}
                 // className="basic-multi-select"
                 // classNamePrefix="select-"
-                styles={colourStyles}
+                // styles={colourStyles}
                 onChange={handleChange}
                 name={name}
             />
